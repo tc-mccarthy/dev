0b63b0b31362b6472f29bab12cddfc02dffe4f3d
TC McCarthy <tc.mccarthy1@gmail.com>
2016-09-06 22:21:14 -0400

    Updates base model. Adds find or create

--- express-shell/models/base.js
+++ express-shell/models/base.js
@@ -80,6 +80,55 @@ var Base = function(table) {
 
         db.update(data, cb);
     };
+
+    this.findOrCreate = function(where, vals, cb) {
+        var _this = this;
+        async.waterfall([
+
+            function(x) {
+                _this.find({
+                    where: where,
+                    columns: ["*"]
+                }, function(results) {
+                    if (typeof results === "undefined" || results.length == 0) {
+                        x(null);
+                    } else {
+                        x("found", results);
+                    }
+                });
+            },
+
+            function(x) {
+                _this.create(vals, function(results) {
+                    x(null, results);
+                });
+            },
+
+            function(results, x) {
+                _this.find({
+                    where: [{
+                        col: "id",
+                        op: "=",
+                        val: results.insertId
+                    }],
+                    columns: ["*"]
+                }, function(results) {
+                    if (typeof results === "undefined" || results.length == 0) {
+                        x(null);
+                    } else {
+                        x("created", results);
+                    }
+                });
+            }
+        ], function(err, data) {
+            if (typeof cb === "function") {
+                cb({
+                    status: err,
+                    data: data
+                });
+            }
+        });
+    }
 };
 
 exports.Base = Base;
