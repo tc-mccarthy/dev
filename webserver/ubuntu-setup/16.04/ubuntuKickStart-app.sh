a2af876bedca0accd7f2934636a7137195cc5fbb
TC McCarthy <tc.mccarthy1@gmail.com>
2018-10-20 11:08:24 -0400

    Updates app script

--- webserver/ubuntu-setup/16.04/ubuntuKickStart-app.sh
+++ webserver/ubuntu-setup/16.04/ubuntuKickStart-app.sh
@@ -5,16 +5,18 @@
 ## Ubuntu Server              ####
 ##                            ####
 ## Author: TC McCarthy        ####
-## Date: March 8, 2017       ####
+## Date: Oct. 20, 2018        ####
 ##################################
+
+#### PHP REPO ####
+sudo add-apt-repository -qy ppa:ondrej/php
+wget -qO- https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
 ##### FIRST GET ALL OF THE LATEST FILES #####
 apt-get -qy update && apt-get -qy upgrade
 
 #### THEN INSTALL THE BASICS ####
-apt-get -y install screen vim ssh nginx mysql-server php7.0-fpm php7.0-curl php7.0-mysql php7.0-gd build-essential libcurl4-openssl-dev libxml2-dev libfuse-dev comerr-dev libfuse2 libidn11-dev libkrb5-dev libldap2-dev libselinux1-dev libsepol1-dev pkg-config memcached php-memcached htop postfix unzip
+apt-get -y install screen vim ssh nginx mysql-server php7.1 php7.1-cli php7.1-common php7.1-json php7.1-opcache php7.1-mysql php7.1-mbstring php7.1-mcrypt php7.1-zip php7.1-fpm build-essential libcurl4-openssl-dev libxml2-dev libfuse-dev comerr-dev libfuse2 libidn11-dev libkrb5-dev libldap2-dev libselinux1-dev libsepol1-dev pkg-config memcached php-memcached htop postfix unzip nodejs
 
 ### THEN INSTALL NODE AND DEPENDENCIES ###
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-sudo apt-get install -y nodejs
 npm install -g pm2
