cbbb0196455b6dfe29330cbd46e8c2ade64349eb
TC McCarthy <tc.mccarthy@newsday.com>
2021-02-03 10:14:08 -0500

    Updates react shell

---
+++ grunt-shell-es6-react/assets/js/components/example.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Example extends Component{
+  render(){
+    return <div>CONTENT HERE</div>
+  }
+}
+
+module.exports = Example;
